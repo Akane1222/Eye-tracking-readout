9952b9a9a821240d19eca6e5148b7b2631d3ee9d
幻中燈 <86186861+Akane1222@users.noreply.github.com>
2023-04-23 15:45:24 +0900

    メイン

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ from PIL import Image
 tesseract_path = r"C:\Program Files\Tesseract-OCR"
 if tesseract_path not in os.environ["PATH"].split(os.pathsep):
     os.environ["PATH"] += os.pathsep + tesseract_path
+
+#OCRエンジンを取得
+tools = pyocr.get_available_tools() 
+if len(tools) == 0:
+    print("OCRエンジンが指定されていません")
+    sys.exit(1)
+else:
+    tool = tools[0]
+
+#画像の読み込み
+img = Image.open("")
+img.show()
+

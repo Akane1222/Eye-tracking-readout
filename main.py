961d862ae56ea157a6c734a9c04095bf49c2707f
幻中燈 <86186861+Akane1222@users.noreply.github.com>
2023-05-26 11:22:23 +0900

    メインファイル

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import os
 import sys
 import pyocr
 from PIL import Image
+import pyopenjtalk as pjt
+import wave
+
+def text_to_speech(text, output_file):
+    labels = pjt.extract_fullcontext(text)
+    wave_data = pjt.synthesize(labels)[0].tobytes()
+    with wave.open(output_file, 'wb') as f:
+        f.setparams((1, 2, 44100, 0, 'NONE', 'not compressed'))
+        f.writeframes(wave_data)
+
 
 #tesseractの指定
 tesseract_path = r"C:\Program Files\Tesseract-OCR"
@@ -18,8 +28,7 @@ else:
     tool = tools[0]
 
 #画像の読み込み
-img = Image.open(r"ここにパスを入力")
-img.show()
+img = Image.open(r"IMG_7053.png")
 
 #文字を読み取り
 builder = pyocr.builders.TextBuilder(tesseract_layout=6)
@@ -27,3 +36,6 @@ result = tool.image_to_string(img,lang="jpn",builder=builder)
 
 #結果を出力
 print(result)
+
+# 音声ファイルを保存して再生
+text_to_speech(bytes(result, encoding='utf-8'), "output.wav")
